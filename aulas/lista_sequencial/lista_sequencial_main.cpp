08ba403071a407676ee98184c26b58bf89734eb1
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-04-03 13:15:46 -0300

    Update lista_sequencial_main.cpp

--- aulas/lista_sequencial/lista_sequencial_main.cpp
+++ aulas/lista_sequencial/lista_sequencial_main.cpp
@@ -2,11 +2,11 @@
 
 int main() {
 	ListSeq ls(5);
+	ls.add(1);
+	ls.add(2);
+	ls.add(4);
 	ls.add(5);
-	ls.add(9);
-	ls.add(15);
-	ls.add(7);
-	ls.insert(100, 1);
+	ls.addSorted(3);
 	ls.print();
 	return 0;
 };
