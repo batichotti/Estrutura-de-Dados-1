9cd8668a727613c9fe8ea8ed0dfa68783d169986
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-05-23 16:38:46 -0300

    Update ordenarBin.cpp

--- prova02/ordenarBin.cpp
+++ prova02/ordenarBin.cpp
@@ -3,13 +3,15 @@
 
 using namespace std;
 
-void SelectionSort(vector<int>& vetor){
-    int size = vetor.size();
-    for (int i = 0; i < size; i++){
+void selectionSort(vector<int>& vetor){
+    for (int i = 0; i < (int) vetor.size(); i++){
         int min = vetor[i];
         int min_pos = -1;
-        for (int j = i+1; j < size; j++){
-            if (vetor.at(j) < min) min = vetor[j]; min_pos = j;
+        for (int j = i+1; j < (int) vetor.size(); j++){
+            if (vetor.at(j) < min){
+                min = vetor[j];
+                min_pos = j;
+            }
         }
         int temp = vetor[i];
         vetor[i] = min;
@@ -18,8 +20,8 @@ void SelectionSort(vector<int>& vetor){
 }
 
 int main(){
-    vector<int> v = {3, 4, 3, 2, 3, 1};
-    SelectionSort(v);
+    vector<int> v = {3, 4, 5, 2, 3, 1};
+    selectionSort(v);
     for (int el : v) cout << el << endl;
 
     return 0;
