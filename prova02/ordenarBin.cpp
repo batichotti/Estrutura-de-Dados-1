3bfe2bb9840e29ea0745a4b220f222f84d8d6af5
ArthurvMachado <avm.arthurv@gmail.com>
2025-05-23 17:10:11 -0300

    Update ordenarBin.cpp

--- prova02/ordenarBin.cpp
+++ prova02/ordenarBin.cpp
@@ -3,26 +3,49 @@
 
 using namespace std;
 
-void selectionSort(vector<int>& vetor){
-    for (int i = 0; i < (int) vetor.size(); i++){
-        int min = vetor[i];
+void selectionSort(vector<int>& vec){
+    for (int i = 0; i < (int) vec.size(); i++){
+        int min = vec[i];
         int min_pos = -1;
-        for (int j = i+1; j < (int) vetor.size(); j++){
-            if (vetor.at(j) < min){
-                min = vetor[j];
+        
+        for (int j = i+1; j < (int) vec.size(); j++){
+            if (vec[j] < min){
+                min = vec[j];
                 min_pos = j;
             }
         }
-        int temp = vetor[i];
-        vetor[i] = min;
-        vetor[min_pos] = temp;
+
+        int temp = vec[i];
+        vec[i] = min;
+        vec[min_pos] = temp;
     }
 }
 
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(vector<int>& vec){
+    for(int i = 0; i < (int) vec.size(); i++) 
+        for(int j = 0; j + 1 < (int) vec.size() - i; j++) 
+            if(vec[j] > vec[j + 1]) swap(vec[j], vec[j + 1]);
+}
+
 int main(){
-    vector<int> v = {3, 4, 5, 2, 3, 1};
-    selectionSort(v);
-    for (int el : v) cout << el << endl;
+    vector<int> selS = {3, 4, 5, 2, 3, 1};
+    selectionSort(selS);
+
+    cout << endl;
+    for (int el : selS) cout << el << " ";
+    cout << endl; cout << endl;
+
+    vector<int> bubS = {3, 4, 5, 2, 3, 1};
+    bubbleSort(bubS);
+    
+    for (int el : bubS) cout << el << " ";
+    cout << endl; cout << endl;
 
     return 0;
 }
