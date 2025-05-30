7a831e5862e736ac69bb0a4a7cf46c73792f3175
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-05-30 16:45:27 -0300

    Update 1.cpp

--- aulas/recursividade/1.cpp
+++ aulas/recursividade/1.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <string>
 
-void print_recursivo(char* txt, int n){
-    
-}
-
-void print_txt(char* txt){
-    print_recursivo(txt, 10);
+void print_txt(std::string txt, int n){
+    if (n > 0){
+        std::cout << txt << std::endl;
+        print_txt(txt, --n);
+    }
+    return;
 }
 
 int main(int argc, char const *argv[]){
-    print_txt("tottenham campeao da europa legue");
+    print_txt("tottenham campeao da europa legue", 10);
     return 0;
 }
