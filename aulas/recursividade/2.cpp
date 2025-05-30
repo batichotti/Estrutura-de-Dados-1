da5bf6d4ff45103cad837f4b32ded03133b7a5cd
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-05-30 16:45:31 -0300

    Update 2.cpp

--- aulas/recursividade/2.cpp
+++ aulas/recursividade/2.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+void count_50(int n){
+    if (n <= 50){
+        std::cout << n << std::endl;
+        count_50(++n);
+    }
+    return;
+}
+
+void count(int num){
+    if (num > 0) {
+        count_50(0);
+        count(--num);
+    }
+    return;
+}
+
 int main(int argc, char const *argv[]){
-    
+    count(1);
     return 0;
 }
