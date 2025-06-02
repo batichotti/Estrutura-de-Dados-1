878b0308c37e7dd44b7246c9515e651f97560039
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-02 14:25:52 -0300

    Update 5.cpp

--- aulas/recursividade/5.cpp
+++ aulas/recursividade/5.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 
 void print_even(std::vector<int>& vec, int idx){
-    if(idx < vec.size()){
+    if(idx >= 0){
         if (!(vec[idx] & 1)) std::cout << vec[idx] << " ";
-        print_even(vec, ++idx);
+        print_even(vec, --idx);
     }
     else std::cout << std::endl;
     return;
@@ -12,6 +12,6 @@ void print_even(std::vector<int>& vec, int idx){
 
 int main(int argc, char const *argv[]){
     std::vector<int> vec = std::vector<int>{1, 2, 3, 4};
-    print_even(vec, 0);
+    print_even(vec, vec.size()-1);
     return 0;
 }
