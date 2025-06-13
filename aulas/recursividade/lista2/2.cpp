0078dd7c1fd141291eff22a28dae9de69e387b6d
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-13 17:16:27 -0300

    Create 2.cpp

---
+++ aulas/recursividade/lista2/2.cpp
@@ -0,0 +1,12 @@
+#include <list>
+#include <iostream>
+using namespace std;
+
+int product(list<int>& lst, list<int>::iterator it){
+    
+}
+
+int main(int argc, char const *argv[]){
+
+    return 0;
+}
