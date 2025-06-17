61307f4377706a69871ec96ab68326c91c182828
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-17 14:03:40 -0300

    Update 2.cpp

--- aulas/recursividade/lista2/2.cpp
+++ aulas/recursividade/lista2/2.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 int product(list<int>& lst, list<int>::iterator it){
-    
+    if(it == lst.end()) return 1;
+    return (*it) * product(lst, next(it));
 }
 
 int main(int argc, char const *argv[]){
-
+    list<int> lst = {2, 3, 4};
+    int result = product(lst, lst.begin());
+    cout << "Produto: " << result << endl;
     return 0;
 }
