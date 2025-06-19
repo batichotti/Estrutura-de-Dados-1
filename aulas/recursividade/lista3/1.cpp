ede26e9363fe1cc5ca4ddca67305804750161fd5
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-19 18:42:08 -0300

    lista3

---
+++ aulas/recursividade/lista3/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+float power(int base, int exp){
+    if (exp == 0) return 1;
+    if (exp < 0) return 1/(power(base, -exp));
+    return (exp%2)?(base * power(base, (int) exp/2) * power(base, (int) exp/2)):(power(base, (int) exp/2) * power(base, (int) exp/2)); 
+}
+
+int main(int argc, char const *argv[]){
+    int base, exp;
+    cout << "Digite a base: ";
+    cin >> base;
+    cout << "Digite o expoente: ";
+    cin >> exp;
+    cout << "Resultado: " << power(base, exp) << endl;
+    return 0;
+}
---
+++ aulas/recursividade/lista3/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int mdc(int a, int b){
+    
+}
+
+int main(int argc, char const *argv[]){
+    
+    return 0;
+}
