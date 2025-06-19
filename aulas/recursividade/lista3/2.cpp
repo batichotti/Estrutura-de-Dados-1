3bae142b23ebbd5c0a36fc3fefd08ed31d9a6b58
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-19 19:06:26 -0300

    Update 2.cpp

--- aulas/recursividade/lista3/2.cpp
+++ aulas/recursividade/lista3/2.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int mdc(int a, int b){
-    
+    return (b==0)?(a):(mdc(b, a%b));
 }
 
 int main(int argc, char const *argv[]){
-    
+    int a, b;
+    cout << "Digite dois numeros: ";
+    cin >> a >> b;
+    cout << "MDC(" << a << ", " << b << ") = " << mdc(a, b) << endl;
     return 0;
 }
