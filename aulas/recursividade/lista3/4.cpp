80c98c6ae13494d1347918084e3a1244188191b1
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-20 13:08:58 -0300

    Create 4.cpp

---
+++ aulas/recursividade/lista3/4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void subconjuntos(int arr[], int n, int pos, int pos_swap){
+    if (pos = n-1){
+        
+    }
+}
+
+int main(int argc, char const *argv[]){
+    
+    return 0;
+}
