62112beb7530478ec5bc0d240ef20f3f4c166b90
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-22 11:00:01 -0300

    Create 10.cpp

---
+++ aulas/recursividade/lista3/10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+bool palindromo(string str,int left, int right){
+    if (left >= right) return true;
+    return (str[left] != str[right])?(false):(palindromo(str, left+1, right-1));
+}
+
+int main(int argc, char const *argv[]){
+    string s;
+    cin >> s;
+    if (palindromo(s, 0, s.size() - 1))
+        cout << "SIM" << endl;
+    else
+        cout << "NAO" << endl;
+    return 0;
+}
